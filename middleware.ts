575ad23d7cf4d08b5c1106046b6015c61a5ea89d
SHYAM SAI TEJA <ee21b127@smail.iitm.ac.in>
2024-05-24 10:18:53 +0530

    redirection

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,31 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up", "/"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isPublicRoute(req) && auth().userId) {
+    let path = "/select-org";
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+    const org = new URL(path, req.url);
+    return NextResponse.redirect(org);
+  }
+  if (!isPublicRoute(req) && !auth().userId) {
+    const org = new URL("/sign-in", req.url);
+    return NextResponse.redirect(org);
+  }
+
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/select-org"
+  ) {
+    const org = new URL("/select-org", req.url);
+    return NextResponse.redirect(org);
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
